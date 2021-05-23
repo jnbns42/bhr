51ec98ec82fc567bab803a9be2a74e0687930150
Jonathan Baines <jbainesmccann@bitbucket.org>
2021-05-23 20:34:13 +0100

    icon added to file list, breakpoints updated

--- src/App.js
+++ src/App.js
@@ -2,20 +2,48 @@ import './App.css';
 
 import Nav from './components/nav';
 import FileList from './components/filelist';
+import { useEffect, useRef } from 'react';
 
 function App() {
+
+  const sidebar = useRef();
+
+  const handleNavClose = () => {
+    sidebar.current.classList.add('opacity-0', '-z-50');
+    sidebar.current.classList.remove('opacity-100', 'z-50');
+
+  }
+
+  useEffect(() => {
+      let componentMounted = true;
+      if (componentMounted) {
+        document.addEventListener('navopen', () => {
+          sidebar.current.classList.add('opacity-100', 'z-50');
+          sidebar.current.classList.remove('opacity-0', '-z-50');
+        });
+      }
+      return () => {
+        componentMounted = false;
+      }
+  }, []);
+
   return (
     <div className="App">
       <div class="flex">
-        <aside className="fixed inset-0 z-50 sm:static opacity-0 sm:opacity-1 sm:block bg-white sm:border-r-2 border-gray-200 w-full sm:w-72 py-3 sm:min-h-screen">
-          <div>
-            <a href="/">
+        <aside ref={sidebar} className="fixed inset-0 -z-50 lg:z-50 lg:static opacity-0 lg:opacity-100 lg:block bg-white lg:border-r-2 border-gray-200 w-full lg:w-72 pb-3 lg:min-h-screen transition duration-500">
+          <div className="border-b-2 border-gray-200 lg:border-0 flex align-center justify-between">
+            <a href="/" className="pt-3 pb-3">
               <svg height='50' viewBox='0 0 177 100' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M88 32.5C88 42.165 80.165 50 70.5 50H53V32.5C53 22.835 60.835 15 70.5 15C80.165 15 88 22.835 88 32.5Z' fill='#17CF97'/><path d='M88 67.5C88 57.835 95.835 50 105.5 50H123V67.5C123 77.165 115.165 85 105.5 85C95.835 85 88 77.165 88 67.5Z' fill='#17CF97'/><path d='M53 67.5C53 77.165 60.835 85 70.5 85H88V67.5C88 57.835 80.165 50 70.5 50C60.835 50 53 57.835 53 67.5Z' fill='#17CF97'/><path d='M123 32.5C123 22.835 115.165 15 105.5 15H88V32.5C88 42.165 95.835 50 105.5 50C115.165 50 123 42.165 123 32.5Z' fill='#17CF97'/></svg>
             </a>
+            <button className="border-l-2 py-3 px-6 lg:hidden" onClick={() => {handleNavClose()}}>
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
           </div>
           <Nav/>
         </aside>
-        <main className="bg-gray-100 min-h-screen w-full sm:w-4/5 sm:px-24">
+        <main className="bg-gray-100 min-h-screen w-full lg:px-24">
             <FileList/>
         </main>  
       </div>
--- src/components/filelist.js
+++ src/components/filelist.js
@@ -1,11 +1,11 @@
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import files from '../files.json';
 import Listing from './listing';
 
 function FileList() {
 
 
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState(null);
 
     const [filteredList, setFilteredList] = useState(files);
 
@@ -18,25 +18,32 @@ function FileList() {
         setFilteredList(filtered);
     }
 
+    const navOpen = new Event('navopen');
+
+    const handleNavOpen = () => {
+        document.dispatchEvent(navOpen);
+        console.log('open please');
+    }
+
     return(
         <>
-            <div className="bg-white sm:bg-gray-100 sm:pt-4 sm:pb-3 mb-6 border-b-2 border-gray-200">
+            <div className="bg-white lg:bg-gray-100 lg:pt-4 lg:pb-3 mb-6 border-b-2 border-gray-200">
                 <div className="flex items-center">
-                    <div className="sm:hidden w-18">
-                        <button className="px-6 py-6 border-r-2">
+                    <div className="lg:hidden w-18">
+                        <button className="px-6 py-6 border-r-2" onClick={() => handleNavOpen()}>
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
                             </svg>
                         </button>
                     </div>
-                    <div className="flex-grow bg-white rounded-md flex w-1/2 sm:w-auto space-x-3 px-4 py-2 items-center">
+                    <div className="flex-grow bg-white rounded-md flex w-1/2 lg:w-auto space-x-3 px-4 py-2 items-center">
                         <svg width="15" height="15" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M19 19L13 13L19 19ZM15 8C15 8.91925 14.8189 9.82951 14.4672 10.6788C14.1154 11.5281 13.5998 12.2997 12.9497 12.9497C12.2997 13.5998 11.5281 14.1154 10.6788 14.4672C9.82951 14.8189 8.91925 15 8 15C7.08075 15 6.1705 14.8189 5.32122 14.4672C4.47194 14.1154 3.70026 13.5998 3.05025 12.9497C2.40024 12.2997 1.88463 11.5281 1.53284 10.6788C1.18106 9.82951 1 8.91925 1 8C1 6.14348 1.7375 4.36301 3.05025 3.05025C4.36301 1.7375 6.14348 1 8 1C9.85652 1 11.637 1.7375 12.9497 3.05025C14.2625 4.36301 15 6.14348 15 8Z" stroke="#697888" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                        <input type="text" name="search" className="w-3/4 sm:w-auto sm:flex-grow" value={term} placeholder="Search" onChange={e => handleChange(e.target.value)}/>
+                        <input type="text" name="search" className="w-3/4 lg:w-auto lg:flex-grow" value={term} placeholder="Search" onChange={e => handleChange(e.target.value)}/>
                     </div>
-                    <div className="flex space-x-3 p-2 sm:py-0 sm:pl-4 sm:space-x-4">
-                        <a href="#" className="rounded-full sm:bg-gray-200 block overflow-hidden">
+                    <div className="flex space-x-3 p-2 lg:py-0 lg:pl-4 lg:space-x-4">
+                        <a href="#" className="rounded-full lg:bg-gray-200 block overflow-hidden">
                             <svg className="stroke-current text-gray-500" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="none" viewBox="-2 -2 28 28" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                             </svg>
@@ -47,13 +54,13 @@ function FileList() {
                     </div>
                 </div>
             </div>
-            <div className="px-6 sm:px-0">
+            <div className="px-6 lg:px-0">
                 <h1 className="text-2xl font-bold mb-6">Documents</h1>
 
                 <div className="pb-3 border-b-2 border-gray-200 flex space-x-6">
                     <div className="font-medium flex-grow">Name</div>
-                    <div className="hidden sm:block font-medium w-48">Date Added</div>
-                    <div className="hidden sm:block font-medium w-24">Size</div>
+                    <div className="hidden lg:block font-medium w-48">Date Added</div>
+                    <div className="hidden lg:block font-medium w-24">Size</div>
                 </div>
                 <div>
                     {
